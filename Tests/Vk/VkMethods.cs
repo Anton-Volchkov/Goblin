3237f64d7934fcf1dfe62ae66307dec6bdec9243
equus <kirill.kuzn98@gmail.com>
2019-01-05 20:49:06 +0300

    some rework vk api

--- Tests/Vk/VkMethods.cs
+++ Tests/Vk/VkMethods.cs
@@ -9,22 +9,22 @@ namespace Tests.Vk
         [Fact]
         public async Task GetUsername_Valid_String()
         {
-            var result = await Users.GetUserName(1);
+            var result = await Api.Users.GetUserName(1);
             Assert.False(string.IsNullOrEmpty(result));
         }
 
         [Fact]
         public async Task GetUsername_NotValid_EmptyString()
         {
-            var result = await Users.GetUserName(0);
+            var result = await Api.Users.GetUserName(0);
             Assert.True(string.IsNullOrEmpty(result));
         }
 
         [Fact]
         public async Task SendMessage_Correct()
         {
-            await Messages.Send(157312383, "test", new[] {"photo-146048760_456239017"});
-            await Messages.Send(new long[] { 157312383, 366305213 }, "test",
+            await Api.Messages.Send(157312383, "test solo", new[] {"photo-146048760_456239017"});
+            await Api.Messages.Send(new long[] { 157312383, 366305213 }, "test multi",
                 new[] { "photo-146048760_456239017" });
         }
     }
