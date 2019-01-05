f7c9db85a80d68a38ba517b369bfd137689e5c4e
equus <kirill.kuzn98@gmail.com>
2019-01-05 20:26:13 +0300

    move admins to db

--- Goblin/Bot/Settings.cs
+++ Goblin/Bot/Settings.cs
@@ -4,11 +4,5 @@
     {
         public const string ConfirmationToken = "***REMOVED***";
         public const string AccessToken = "***REMOVED***";
-        public static long[] Developers = { 157312383 }; // TODO: вынести в бд?
-
-        static Settings()
-        {
-            Vk.Api.SetAccessToken(AccessToken);
-        }
     }
 }
