eebb4968d69f0ea6f3dc5b0b5f1ab21519c82947
equus <kirill.kuzn98@gmail.com>
2019-01-08 14:58:12 +0300

    add week day to schedule/reminds rename Api to VkApi

--- Tests/Vk/VkMethods.cs
+++ Tests/Vk/VkMethods.cs
@@ -9,22 +9,22 @@ namespace Tests.Vk
         [Fact]
         public async Task GetUsername_Valid_String()
         {
-            var result = await Api.Users.GetUserName(1);
+            var result = await VkApi.Users.GetUserName(1);
             Assert.False(string.IsNullOrEmpty(result));
         }
 
         [Fact]
         public async Task GetUsername_NotValid_EmptyString()
         {
-            var result = await Api.Users.GetUserName(0);
+            var result = await VkApi.Users.GetUserName(0);
             Assert.True(string.IsNullOrEmpty(result));
         }
 
         [Fact]
         public async Task SendMessage_Correct()
         {
-            await Api.Messages.Send(157312383, "test solo", new[] {"photo-146048760_456239017"});
-            await Api.Messages.Send(new long[] { 157312383, 366305213 }, "test multi",
+            await VkApi.Messages.Send(157312383, "test solo", new[] {"photo-146048760_456239017"});
+            await VkApi.Messages.Send(new long[] { 157312383, 366305213 }, "test multi",
                 new[] { "photo-146048760_456239017" });
         }
     }
