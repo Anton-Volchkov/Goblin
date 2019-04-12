678d83824f87c74f062687a3b7de6d8b986da54e
equus <kirill.kuzn98@gmail.com>
2019-04-12 22:37:15 +0300

    Добавить контроллер для управления юзерами

---
+++ Goblin.WebUI/Controllers/UsersController.cs
@@ -0,0 +1,45 @@
+﻿using Goblin.Application.Users.Commands.DeleteUser;
+using Goblin.Application.Users.Commands.UpdateUser;
+using Goblin.Application.Users.Queries.GetAllUsers;
+using Goblin.Application.Users.Queries.GetUserByVkId;
+using Goblin.Domain.Entities;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Goblin.WebUI.Controllers
+{
+    [Route("api/[controller]")]
+    public class UsersController : Controller
+    {
+        private readonly IMediator _mediator;
+
+        public UsersController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<UsersViewModel>> Get()
+        {
+            return await _mediator.Send(new GetAllUsersQuery());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<User>> GetByVkId(int id)
+        {
+            return await _mediator.Send(new GetUserByVkIdQuery { VkId = id });
+        }
+
+        public async Task<ActionResult<bool>> UpdateUser([FromBody] UpdateUserCommand command)
+        {
+            return await _mediator.Send(command);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<bool>> DeleteByVkId(int id)
+        {
+            return await _mediator.Send(new DeleteUserCommand { VkId = id });
+        }
+    }
+}
