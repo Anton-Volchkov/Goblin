9a97e8429ca1e50edfdef24344ac66451753db9e
equus <kirill.kuzn98@gmail.com>
2019-04-12 22:25:02 +0300

    Добавить получение юзера по VkId

---
+++ Goblin.Application/Users/Queries/GetUserByVkId/GetUserByVkIdQuery.cs
@@ -0,0 +1,10 @@
+﻿using Goblin.Domain.Entities;
+using MediatR;
+
+namespace Goblin.Application.Users.Queries.GetUserByVkId
+{
+    public class GetUserByVkIdQuery : IRequest<User>
+    {
+        public int VkId { get; set; }
+    }
+}
---
+++ Goblin.Application/Users/Queries/GetUserByVkId/GetUserByVkIdQueryHandler.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Goblin.Domain.Entities;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Goblin.Application.Users.Queries.GetUserByVkId
+{
+    public class GetUserByVkIdQueryHandler : IRequestHandler<GetUserByVkIdQuery, User>
+    {
+        private readonly IMainContext _context;
+
+        public GetUserByVkIdQueryHandler(IMainContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<User> Handle(GetUserByVkIdQuery request, CancellationToken cancellationToken)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(x => x.VkId == request.VkId,
+                                                          cancellationToken);
+
+            if(user is null)
+            {
+                return null;
+
+                //TODO THROW
+            }
+
+            return user;
+        }
+    }
+}
