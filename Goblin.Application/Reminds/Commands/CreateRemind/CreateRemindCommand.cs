623385e77df2b005b76d2888c141f9f143cd3405
equus <kirill.kuzn98@gmail.com>
2019-04-12 22:51:49 +0300

    Добавить команду для добавления напоминания

---
+++ Goblin.Application/Reminds/Commands/CreateRemind/CreateRemindCommand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using MediatR;
+
+namespace Goblin.Application.Reminds.Commands.CreateRemind
+{
+    public class CreateRemindCommand : IRequest<bool>
+    {
+        public int VkId { get; set; }
+        public DateTime Date { get; set; }
+        public string Text { get; set; }
+    }
+}
---
+++ Goblin.Application/Reminds/Commands/CreateRemind/CreateRemindCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Goblin.Domain.Entities;
+using MediatR;
+
+namespace Goblin.Application.Reminds.Commands.CreateRemind
+{
+    public class CreateRemindCommandHandler : IRequestHandler<CreateRemindCommand, bool>
+    {
+        private readonly IMainContext _context;
+
+        public CreateRemindCommandHandler(IMainContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<bool> Handle(CreateRemindCommand request, CancellationToken cancellationToken)
+        {
+            await _context.Reminds.AddAsync(new Remind()
+            {
+                UserId = request.VkId,
+                Time = request.Date,
+                Text = request.Text
+            }, cancellationToken);
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return true;
+        }
+    }
+}
